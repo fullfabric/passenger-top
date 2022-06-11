21fe3254567b3c3ea000aafd35896d848fe77b99
luis-ca <luis.ca@gmail.com>
2022-06-11 22:30:41 +0100

    added slow requests, added pid, state

--- lib/passenger_top/formatter.rb
+++ lib/passenger_top/formatter.rb
@@ -6,17 +6,22 @@ module PassengerTop
     end
 
     def simple
-      output = @data.map do |client_id, client|
+      @data.map do |client_id, client|
         {
           thread: client["thread_name"],
           client_id: client_id,
-          # state: client["connection_state"],
+          pid: client["current_request"]["session"]["pid"],
+          state: client["connection_state"],
           method: client["current_request"]["method"],
           host: client["current_request"]["host"],
           path: client["current_request"]["path"],
           duration: -client["connected_at"]["relative_timestamp"].round(2)
         }
       end
+    end
+
+    def slow
+      simple.select { |row| row[:duration] > 5 }
     end
 
   end
--- spec/lib/passenger_top/formatter_spec.rb
+++ spec/lib/passenger_top/formatter_spec.rb
@@ -1,15 +1,23 @@
 describe PassengerTop::Formatter do
 
-  it "formats output" do
-    json = File.read("./spec/support/output_1")
-    hash = PassengerTop::Parser.parse!(json)
+  let!(:contents) { File.read("./spec/support/output_1") }
+  let!(:data) { PassengerTop::Parser.parse!(json) }
 
-    formatter = PassengerTop::Formatter.new(hash)
+  let!(:formatter) { PassengerTop::Formatter.new(data) }
+
+  it "formats output" do
     output = formatter.simple
 
     expect(output).to be_a(Array)
     expect(output.size).to eq 2
-    expect(output[0].keys).to eq([:thread, :client_id, :method, :host, :path, :duration])
+
+    expected_keys = [:thread, :client_id, :pid, :state, :method, :host, :path, :duration]
+    expect(output[0].keys).to eq(expected_keys)
+  end
+
+  it "returns slow requests" do
+    output = formatter.slow
+    expect(output.size).to eq 1
   end
 
 end
